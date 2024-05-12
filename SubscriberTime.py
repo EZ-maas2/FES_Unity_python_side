34ebbf8500a8262d191f636b20f5e73d81902064
EZ-maas2 <85937429+EZ-maas2@users.noreply.github.com>
2024-05-12 09:03:24 +0200

    Better calibration, 2 separate ports

--- SubscriberTime.py
+++ SubscriberTime.py
@@ -1,8 +1,6 @@
 
 import zmq
-from FES_classes.stim_mid import  Stimulation_Mid_Lvl
-from FES_classes.fes import FES
-import  datetime
+import datetime
 
 def setupSUB(ip, port, topic = "FES"):
     cont = zmq.Context()
@@ -14,68 +12,18 @@ def setupSUB(ip, port, topic = "FES"):
     return socket
 
 
-def check_message(message, topic):
-    message = message.decode('utf-8')
-
-    if message == topic:
-        return 0
-    elif 'True' in message:
-        return True
-    else:
-        return False
-
-
-def check_message_stimulation(message, topic):
-    if message == topic:
-        return 0
-    elif 'channel' in message: # message is of the format channel|red|
-        stim_params_list = message.split('|')
-        return create_stim(stim_params_list)
-    else:
-        return 0
-
-
-def create_stim(stimulation_params):
-    channel_ix = stimulation_params.index('channel')
-    pwm_ix = stimulation_params.index('pwm_micros')
-    amplitude_ix = stimulation_params.index('amplitude_mA')
-    period_ix = stimulation_params.index('period_ms')
-
-    channel = stimulation_params[channel_ix+1]
-    pwm = int(stimulation_params[pwm_ix+1])
-    amp = float(stimulation_params[amplitude_ix+1])
-    period = float(stimulation_params[period_ix+1])
-
-    stimulation = Stimulation_Mid_Lvl(amplitude_mA=amp, period_ms=period, pulse_width_micros=pwm, channel=channel)
-    return stimulation
-
-
-def setupFES(port):
-    fes_device = FES(port)
-    fes_device.mid_lvl_init()
-    return fes_device
-
 
 def data_registration_setup():
-    print("Please provide participant name:")
-    name = input()
-    name = name.split()
-    full_name = ''
-    for part in name:
-        full_name += part
-
-    print("Please provide the experiment mode (FES or Control):")
-    mode = input()
-    print("Please provide the speed of the box: ")
-    speed = input()
-    return full_name+mode+speed
+    print("Please provide  filename:")
+    filename = input()
+    return filename
 
 if __name__ == "__main__":
-    #fes_device = setupFES(port='COM7')
-    ip = '192.168.178.85' # home ip for vr
+    #ip = '192.168.178.85' # home ip for vr
     ip = '192.168.178.101' # home ip for desktop
+    #ip = '10.158.101.242'  # lrz headset ip address
     topic = 'Timer'
-    port = 5556
+    port = 5557
     socket = setupSUB(ip, port, topic) # let's try a different format
     experiment_id = data_registration_setup()
 
@@ -85,7 +33,8 @@ if __name__ == "__main__":
         message = socket.recv()
         message = message.decode('utf-8')
         print(f'{i}: {message}')
-        with open(f"{experiment_id}", 'w') as file:
-            file.write(f"{experiment_id}_i : {message} s")
+        if message != topic:
+            with open(f"Data/{experiment_id}", 'a') as file:
+                file.write(f"{datetime.datetime.now()}: {experiment_id}_{i} : {message} s \n")
         i += 1
     print('Stopped!')
